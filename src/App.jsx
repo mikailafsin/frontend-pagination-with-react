dd4a218ff080d6ff3e374e5a2f938fe388f19d34
mikailafsin <mikailwnchstr@gmail.com>
2024-04-05 16:40:11 +0300

    states are defined.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function App() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(10);
+
     return <>Hello, World!</>;
 }
 
